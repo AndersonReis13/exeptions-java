2f3fd22e16d061c509d727e5f7767c2a72c2cb52
Anderson Silva <andersonline62@gmail.com>
2024-02-14 19:07:34 -0300

    Vary bad solution

--- src/application/Project.java
+++ src/application/Project.java
@@ -1,5 +1,47 @@
 package application;
 
-public class Project {
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
 
+import model.entities.Reservation;
+
+public class Project {
+	public static void main(String args[]) throws ParseException {
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		System.out.print("Room number: ");
+		int number = sc.nextInt();
+		System.out.print("Chack-in date(dd/MM/yyyy) ");
+		Date chakIn = sdf.parse(sc.next()); // Estou fazendo o parse ler o que será contido dentro da data especificada..
+		System.out.print("Chack-in date(dd/MM/yyyy) ");
+		Date chakOut = sdf.parse(sc.next()); 
+		
+		if(!chakOut.after(chakIn)) {
+			System.out.println("Erro in reservation: Chak-out must be after chak-In date");
+		}else {
+			Reservation reservation = new Reservation(number, chakIn, chakOut);
+			System.out.println("Reservation: " + reservation);
+			
+			System.out.println("Enter new Date a reservetion: ");
+			System.out.print("Chack-in date(dd/MM/yyyy) ");
+			chakIn = sdf.parse(sc.next()); 
+			System.out.print("Chack-in date(dd/MM/yyyy) ");
+			chakOut = sdf.parse(sc.next());
+			Date now = new Date();
+			if(chakIn.before(now) || chakOut.before(chakOut) ) {
+				System.out.println("Erro in reservation: Chak-out must be after chak-In date now");
+			}else if(!chakOut.after(chakIn)) {
+				System.out.println("Erro in reservation: Chak-out must be after chak-In date");
+			}else{
+				
+				reservation.UpdateDate(chakIn, chakOut);
+				System.out.println("Reservation: " + reservation);
+
+			}	
+		}
+	}
 }
+
--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -1,5 +1,64 @@
 package model.entities;
 
-public class Reservation {
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
+public class Reservation {
+	private Integer roomNumber;
+	private Date chakIn;
+	private Date chakOut;
+	
+	public static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
+	public Reservation() {
+	}
+
+	public Reservation(Integer roomNumber, Date chakIn, Date chakOut) {
+		super();
+		this.roomNumber = roomNumber;
+		this.chakIn = chakIn;
+		this.chakOut = chakOut;
+	}
+
+	public Integer getRoomNumber() {
+		return roomNumber;
+	}
+
+	public void setRoomNumber(Integer roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+	public Date getChakIn() {
+		return chakIn;
+	}
+
+	public Date getChakOut() {
+		return chakOut;
+	}
+
+	
+	public long Duration() {
+		long diff = chakIn.getTime() - chakOut.getTime(); // Com isso tu pega a diferença entre as duas variavel EM MILISEGUNDOS.
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS); // TIME-UNIT É UMA CLASSE DO JAVA QUE FAZ A CONVERÇÃO DOS MILISEGUNDOS/SEGUNDOS/MINUTOS/DIAS EM DIAS OU QUALQUER OUTRA COISA EM RELAÇÃO AO TEMPO.
+	}
+	
+	public void UpdateDate( Date chakIn, Date chakOut) {
+		this.chakIn = chakIn;
+		this.chakOut = chakOut;
+	}
+	
+	@Override
+	public String toString() {
+	return "Room: " 
+			+ roomNumber 
+			+ ", chakIn: "
+			+ sdf.format(chakIn) // Isso representa o modo que estou transformando o chakIn, com "/" e não com "-"
+			+ ", chakOut: "
+			+ sdf.format(chakOut)
+			+ ", duratution: "
+			+ Duration();
+	}
+	
 }
+
